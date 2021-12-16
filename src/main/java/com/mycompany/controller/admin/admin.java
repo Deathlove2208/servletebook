18f027791174bbb9e6b4a7b349dd66b3eb732be9
Khang <19110374@student.hcmute.edu.vn>
2021-12-16 18:48:18 +0700

    Create admin.java

---
+++ src/main/java/com/mycompany/controller/admin/admin.java
@@ -0,0 +1,51 @@
+package com.mycompany.controller.admin;
+
+import com.mycompany.dao.UserDAO;
+import com.mycompany.model.User;
+import com.mycompany.dao.BookDAO;
+import com.mycompany.model.Book;
+import com.mycompany.util.HibernateUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.Query;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+
+
+@WebServlet(name = "admin", urlPatterns = {"/admin"})
+public class admin extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private List<Book> books;
+    private BookDAO bookDAO;
+    public admin() {
+        super();
+        this.bookDAO = new BookDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	this.books = this.bookDAO.getAllBooks();
+        request.setAttribute("books", this.books);
+        RequestDispatcher rd = request.getRequestDispatcher("admin.jsp");
+        rd.forward(request, response);
+    }
+
+    
+ @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        
+    }
+
+
+
+}
