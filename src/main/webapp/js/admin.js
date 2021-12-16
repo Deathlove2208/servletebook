0129eda3679d256c9387bd206a8e7f683cd826f4
Khang <19110374@student.hcmute.edu.vn>
2021-12-16 18:49:10 +0700

    Create admin.js

---
+++ src/main/webapp/js/admin.js
@@ -0,0 +1,9 @@
+const users = document.getElementsByClassName("delete");
+Array.from(users).forEach((user) => {
+		user.onclick = (e) => {
+			if(!confirm("you want to delete?")) {
+				e.preventDefault();
+			}
+		};
+});
+
