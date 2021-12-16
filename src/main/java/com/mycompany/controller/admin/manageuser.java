90ad9f793c200954cfd5bb1079e59ff1149431e2
Khang <19110374@student.hcmute.edu.vn>
2021-12-16 20:30:17 +0700

    Update manageuser.java

--- src/main/java/com/mycompany/controller/admin/manageuser.java
+++ src/main/java/com/mycompany/controller/admin/manageuser.java
@@ -40,10 +40,6 @@ public class manageuser extends HttpServlet {
     
  @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        
+           
     }
-
-
-
 }
